bdd92bef2ca27dcda333e85eda82e31be5d65492
Валера Агеев <valera0118ag@gmail.com>
2026-04-10 09:06:07 +0400

    Исправил баг с загрузочным экраном

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -52,7 +52,6 @@ class _LoadingScreenState extends State<LoadingScreen>
             'assets/images/load_img.png',
             width: 180,
             height: 180,
-            fit: BoxFit.contain,
           ),
         ),
       ),
